462ce3a8623073930281d3d8cab29ef804a31719
Harsh Vadher <vadherharsh0511@gmail.com>
2025-12-05 23:57:30 +0530

    adding first time

---
+++ src/utils/cn.ts
@@ -0,0 +1,8 @@
+//for navbar-menu styling
+
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
