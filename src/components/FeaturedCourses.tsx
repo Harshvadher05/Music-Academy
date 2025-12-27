3a4d6e5662cd3e8b653d5890db9d5e3f73b13e40
Harsh Vadher <vadherharsh0511@gmail.com>
2025-12-27 20:40:51 +0530

    solve warmings

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -53,11 +53,11 @@ function FeaturedCourses() {
                 </div>
 
                 {/* Content section */}
-                <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                <div className="p-4 sm:p-6 flex flex-col items-center text-center grow">
                   <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
                     {course.title}
                   </p>
-                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400 grow">
                     {course.description}
                   </p>
                   <Link
